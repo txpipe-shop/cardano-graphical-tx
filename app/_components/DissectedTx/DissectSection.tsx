92086fe5c3fe5c7a124f5ca87b3b99af7c372206
sofia-bobbiesi <sofiabobbiesi@gmail.com>
2024-10-03 19:20:27 -0300

    Add accordion items

--- app/_components/DissectedTx/DissectSection.tsx
+++ app/_components/DissectedTx/DissectSection.tsx
@@ -2,6 +2,7 @@ import type { IGraphicalTransaction } from "~/app/_interfaces";
 import { JSONBIG } from "~/app/_utils";
 import { EmptyBlock, P, PropBlock, Section } from "./Constructors";
 import TOPICS from "./topics";
+import { Accordion, AccordionItem } from "@nextui-org/accordion";
 
 export function DissectSection({ tx }: { tx: IGraphicalTransaction }) {
   const {
@@ -40,223 +41,293 @@ export function DissectSection({ tx }: { tx: IGraphicalTransaction }) {
 
   return (
     <div className="flex h-screen flex-col gap-0 overflow-auto p-10 pt-32">
-      <h4 className="text-3xl">Valid CBOR data</h4>
-      <P>Your HEX bytes were successfully decoded using the CBOR standard.</P>
-      {propsBlocks.map(({ title, value, description }, i) => (
-        <PropBlock
-          title={title}
-          value={value}
-          description={i == 0 ? TOPICS.tx : description}
-          key={i}
-        />
-      ))}
-
-      <h4 className="text-3xl font-bold text-blue-500 underline decoration-solid underline-offset-2">
-        Transaction Inputs
-      </h4>
-      <P>{TOPICS.inputs}</P>
-      {inputs
-        .filter((i) => !i.isReferenceInput)
-        .map(({ txHash, index }, i) => (
-          <Section key={i} title="Input" titleClass="text-blue-500">
+      <Accordion defaultExpandedKeys={["valid-cbor"]}>
+        <AccordionItem
+          key="valid-cbor"
+          title={<h4 className="text-3xl">Valid CBOR data</h4>}
+          subtitle={
+            <P>
+              Your HEX bytes were successfully decoded using the CBOR standard.
+            </P>
+          }
+          textValue="Transaction Details"
+        >
+          {propsBlocks.map(({ title, value, description }, i) => (
             <PropBlock
-              title="UtxoRef Hash"
-              description={i == 0 ? TOPICS.inputs_hash : ""}
-              value={txHash}
+              title={title}
+              value={value}
+              description={i == 0 ? TOPICS.tx : description}
+              key={i}
             />
-            <PropBlock
-              title="UtxoRef Index"
-              description={i == 0 ? TOPICS.inputs_index : ""}
-              value={index}
-            />
-          </Section>
-        ))}
-      {inputs.filter((i) => !i.isReferenceInput).length === 0 && <EmptyBlock />}
-
-      <h4 className="text-3xl font-bold text-red-500 underline decoration-solid underline-offset-2">
-        Transaction Outputs
-      </h4>
-      <P>{TOPICS.outputs}</P>
-      {outputs.map(({ txHash, index, address, lovelace, assets }, i) => (
-        <Section title="Output" titleClass="text-red-500" key={i}>
-          <PropBlock title="Tx Hash" value={txHash} />
-          <PropBlock title="Output Index" value={index} />
-          <Section title="Address">
-            <PropBlock
-              title="Bech32"
-              value={address?.bech32}
-              description={TOPICS.outputs_address}
-            />
-            <PropBlock title="Header Type" value={address?.headerType} />
-            <PropBlock title="Kind" value={address?.kind} />
-            <PropBlock title="Network Type" value={address?.netType} />
-            <PropBlock title="Payment" value={address?.payment} />
-          </Section>
-          <Section title="Assets">
-            <PropBlock
-              title="Lovelace"
-              value={lovelace}
-              description={TOPICS.outputs_lovelace}
-            />
-            {assets.map(({ policyId, assetsPolicy }, i) => (
-              <Section key={i} title="Assets">
-                <PropBlock title="Policy Id" value={policyId} />
+          ))}
+        </AccordionItem>
+      </Accordion>
+      <Accordion>
+        <AccordionItem
+          title={
+            <h4 className="text-3xl font-bold text-blue-500 underline decoration-solid underline-offset-2">
+              Transaction Inputs
+            </h4>
+          }
+          subtitle={<P>{TOPICS.inputs}</P>}
+          textValue="Transaction Inputs"
+        >
+          {inputs
+            .filter((i) => !i.isReferenceInput)
+            .map(({ txHash, index }, i) => (
+              <Section key={i} title="Input" titleClass="text-blue-500">
+                <PropBlock
+                  title="UtxoRef Hash"
+                  description={i == 0 ? TOPICS.inputs_hash : ""}
+                  value={txHash}
+                />
+                <PropBlock
+                  title="UtxoRef Index"
+                  description={i == 0 ? TOPICS.inputs_index : ""}
+                  value={index}
+                />
+              </Section>
+            ))}
+          {inputs.filter((i) => !i.isReferenceInput).length === 0 && (
+            <EmptyBlock />
+          )}
+        </AccordionItem>
+      </Accordion>
+      <Accordion>
+        <AccordionItem
+          title={
+            <h4 className="text-3xl font-bold text-red-500 underline decoration-solid underline-offset-2">
+              Transaction Outputs
+            </h4>
+          }
+          subtitle={<P>{TOPICS.outputs}</P>}
+          textValue="Transaction Outputs"
+        >
+          {outputs.map(({ txHash, index, address, lovelace, assets }, i) => (
+            <Section title="Output" titleClass="text-red-500" key={i}>
+              <Section title="">
+                <PropBlock title="Tx Hash" value={txHash} />
+              </Section>
+              <Section title="">
+                <PropBlock title="Output Index" value={index} />
+              </Section>
+              <Section title="Address">
+                <PropBlock
+                  title="Bech32"
+                  value={address?.bech32}
+                  description={TOPICS.outputs_address}
+                />
+                <PropBlock title="Header Type" value={address?.headerType} />
+                <PropBlock title="Kind" value={address?.kind} />
+                <PropBlock title="Network Type" value={address?.netType} />
+                <PropBlock title="Payment" value={address?.payment} />
+              </Section>
+              <Section title="Assets">
+                <PropBlock
+                  title="Lovelace"
+                  value={lovelace}
+                  description={TOPICS.outputs_lovelace}
+                />
+                {assets.map(({ policyId, assetsPolicy }, j) => (
+                  <Section key={j} title="Assets">
+                    <PropBlock title="Policy Id" value={policyId} />
+                    {assetsPolicy.map(
+                      ({ assetName, assetNameAscii, amount }, k) => (
+                        <Section title="Asset" key={k}>
+                          <PropBlock title="Asset Name" value={assetName} />
+                          <PropBlock
+                            title="Asset Name (ASCII)"
+                            value={assetNameAscii}
+                          />
+                          <PropBlock title="Amount" value={amount} />
+                        </Section>
+                      ),
+                    )}
+                  </Section>
+                ))}
+              </Section>
+            </Section>
+          ))}
+          {outputs.length === 0 && <EmptyBlock />}
+        </AccordionItem>
+      </Accordion>
+      <Accordion>
+        <AccordionItem
+          title={
+            <h4 className="text-3xl font-bold text-blue-500 underline decoration-dashed underline-offset-2">
+              Reference Inputs
+            </h4>
+          }
+          subtitle={<P>{TOPICS.reference_inputs}</P>}
+          textValue="Reference Inputs"
+        >
+          {inputs
+            .filter((i) => i.isReferenceInput)
+            .map(({ txHash, index }, i) => (
+              <Section title="Input" titleClass="text-blue-500" key={i}>
+                <PropBlock
+                  title="UtxoRef Hash"
+                  value={txHash}
+                  description={i == 0 ? TOPICS.inputs_hash : ""}
+                />
+                <PropBlock
+                  title="UtxoRef Index"
+                  value={index}
+                  description={i == 0 ? TOPICS.inputs_index : ""}
+                />
+              </Section>
+            ))}
+          {inputs.filter((i) => i.isReferenceInput).length === 0 && (
+            <EmptyBlock />
+          )}
+        </AccordionItem>
+      </Accordion>
+      <Accordion>
+        <AccordionItem
+          title={<h4 className="f text-3xl">Certificates</h4>}
+          textValue="Certificates"
+        >
+          {certificates?.map(({ json }, i) => (
+            <Section title="Certificate" titleClass="text-blue-500" key={i}>
+              <PropBlock title="JSON" value={json} />
+            </Section>
+          ))}
+          {certificates?.length === 0 && <EmptyBlock />}
+        </AccordionItem>
+      </Accordion>
+      <Accordion>
+        <AccordionItem
+          title={<h4 className="text-3xl">Withdrawals</h4>}
+          textValue="Withdrawals"
+        >
+          {withdrawals?.map(({ rawAddress, amount }, i) => (
+            <Section title="Withdrawal" key={i}>
+              <PropBlock title="Raw Address" value={rawAddress} />
+              <PropBlock title="Amount" value={amount} />
+            </Section>
+          ))}
+          {withdrawals?.length === 0 && <EmptyBlock />}
+        </AccordionItem>
+      </Accordion>
+      <Accordion>
+        <AccordionItem
+          title={<h4 className="text-3xl">Transaction Mints</h4>}
+          textValue="Transaction Mints"
+          subtitle={<P>{TOPICS.mints}</P>}
+        >
+          {mints.map(({ policyId, assetsPolicy }, i) => {
+            const mint = assetsPolicy.find((a) => a.amount && a.amount > 0);
+            return (
+              <Section title="Policy ID" key={i}>
+                <PropBlock
+                  title="Policy Id"
+                  value={policyId}
+                  color={mint ? "green" : "red"}
+                />
                 {assetsPolicy.map(
                   ({ assetName, assetNameAscii, amount }, i) => (
                     <Section title="Asset" key={i}>
                       <PropBlock title="Asset Name" value={assetName} />
-                      <PropBlock
-                        title="Asset Name (ASCII)"
-                        value={assetNameAscii}
-                      />
-                      <PropBlock title="amount" value={amount} />
+                      <PropBlock title="Asset Name" value={assetNameAscii} />
+                      <PropBlock title="Amount" value={amount} />
                     </Section>
                   ),
                 )}
               </Section>
+            );
+          })}
+          {mints.length === 0 && <PropBlock value="No minting or burning" />}
+        </AccordionItem>
+      </Accordion>
+      <Accordion>
+        <AccordionItem
+          title={<h4 className="text-3xl">Transaction Metadata</h4>}
+          textValue="Transaction Metadata"
+          subtitle={<P>{TOPICS.metadata}</P>}
+        >
+          {metadata?.map(({ label, jsonMetadata }, i) => (
+            <Section title="Metadatum" key={i}>
+              <PropBlock
+                title="Metadata Label"
+                value={label}
+                description={i == 0 ? TOPICS.inputs_hash : ""}
+              />
+              <PropBlock
+                title="Metadatum Value"
+                value={JSONBIG.stringify(jsonMetadata, null, 2)}
+                description={i == 0 ? TOPICS.inputs_index : ""}
+              />
+            </Section>
+          ))}
+          {metadata?.length === 0 && <EmptyBlock />}
+        </AccordionItem>
+      </Accordion>
+      <Accordion>
+        <AccordionItem
+          title={<h4 className="text-3xl">Collateral</h4>}
+          textValue="Collateral"
+        >
+          <blockquote className="">
+            {collateral?.collateralReturn.map(({ txHash, index }, i) => (
+              <blockquote className="" key={i}>
+                <h4 className="text-3xl">Collateral Return</h4>
+                <PropBlock
+                  title="UtxoRef Hash"
+                  value={txHash}
+                  description={i == 0 ? TOPICS.inputs_hash : ""}
+                />
+                <PropBlock
+                  title="UtxoRef Index"
+                  value={index}
+                  description={i == 0 ? TOPICS.inputs_index : ""}
+                />
+              </blockquote>
             ))}
-          </Section>
-        </Section>
-      ))}
-      {outputs.length === 0 && <EmptyBlock />}
-
-      <h4 className="text-3xl font-bold text-blue-500 underline decoration-dashed underline-offset-2">
-        Reference Inputs
-      </h4>
-      <P>{TOPICS.reference_inputs}</P>
-      {inputs
-        .filter((i) => i.isReferenceInput)
-        .map(({ txHash, index }, i) => (
-          <Section title="Input" titleClass="text-blue-500" key={i}>
-            <PropBlock
-              title="UtxoRef Hash"
-              value={txHash}
-              description={i == 0 ? TOPICS.inputs_hash : ""}
-            />
-            <PropBlock
-              title="UtxoRef Index"
-              value={index}
-              description={i == 0 ? TOPICS.inputs_index : ""}
-            />
-          </Section>
-        ))}
-      {inputs.filter((i) => i.isReferenceInput).length === 0 && <EmptyBlock />}
-
-      <h4 className="text-3xl">Certificates</h4>
-      {certificates?.map(({ json }, i) => (
-        <Section title="Certificate" titleClass="text-blue-500" key={i}>
-          <PropBlock title="JSON" value={json} />
-        </Section>
-      ))}
-      {certificates?.length === 0 && <EmptyBlock />}
-
-      <h4 className="text-3xl">Withdrawals</h4>
-      {withdrawals?.map(({ rawAddress, amount }, i) => (
-        <Section title="Withdrawal" key={i}>
-          <PropBlock title="Raw Address" value={rawAddress} />
-          <PropBlock title="Amount" value={amount} />
-        </Section>
-      ))}
-      {withdrawals?.length === 0 && <EmptyBlock />}
-
-      <h4 className="text-3xl">Transaction Mints</h4>
-      <P>{TOPICS.mints}</P>
-      {mints.map(({ policyId, assetsPolicy }, i) => {
-        const mint = assetsPolicy.find((a) => a.amount && a.amount > 0);
-        return (
-          <Section title="Policy ID" key={i}>
-            <PropBlock
-              title="Policy Id"
-              value={policyId}
-              color={mint ? "green" : "red"}
-            />
-            {assetsPolicy.map(({ assetName, assetNameAscii, amount }, i) => (
-              <Section title="Asset" key={i}>
-                <PropBlock title="Asset Name" value={assetName} />
-                <PropBlock title="Asset Name" value={assetNameAscii} />
-                <PropBlock title="Amount" value={amount} />
-              </Section>
-            ))}
-          </Section>
-        );
-      })}
-      {mints.length === 0 && <PropBlock value="No minting or burning" />}
-
-      <h4 className="text-3xl">Transaction Metadata</h4>
-      <P>{TOPICS.metadata}</P>
-      {metadata?.map(({ label, jsonMetadata }, i) => (
-        <Section title="Metadatum" key={i}>
-          <PropBlock
-            title="Metadata Label"
-            value={label}
-            description={i == 0 ? TOPICS.inputs_hash : ""}
-          />
-          <PropBlock
-            title="Metadatum Value"
-            value={JSONBIG.stringify(jsonMetadata, null, 2)}
-            description={i == 0 ? TOPICS.inputs_index : ""}
-          />
-        </Section>
-      ))}
-      {metadata?.length === 0 && <EmptyBlock />}
-
-      <blockquote className="">
-        <h4 className="text-3xl">Collateral</h4>
-        {collateral?.collateralReturn.map(({ txHash, index }, i) => (
-          <blockquote className="" key={i}>
-            <h4 className="text-3xl">Collateral Return</h4>
-            <PropBlock
-              title="UtxoRef Hash"
-              value={txHash}
-              description={i == 0 ? TOPICS.inputs_hash : ""}
-            />
-            <PropBlock
-              title="UtxoRef Index"
-              value={index}
-              description={i == 0 ? TOPICS.inputs_index : ""}
-            />
+            <PropBlock title="Total" value={collateral?.total} />
           </blockquote>
-        ))}
-        <PropBlock title="Total" value={collateral?.total} />
-      </blockquote>
-
-      <h4 className="text-3xl">Witnesses</h4>
-      <P>{TOPICS.witnesses}</P>
-
-      {witnesses?.vkeyWitnesses.map(({ hash, key, signature }, i) => (
-        <Section title="Verification Key Witness" key={i}>
-          <PropBlock
-            title="Key"
-            value={key}
-            description={i == 0 ? TOPICS.vkey_witness : ""}
-          />
-          <PropBlock title="Hash" value={hash} />
-          <PropBlock title="Signature" value={signature} />
-        </Section>
-      ))}
-      {witnesses?.vkeyWitnesses.length === 0 && (
-        <EmptyBlock title="Verification Key Witness" />
-      )}
-
-      {witnesses?.plutusData.map(({ hash, bytes, json }, i) => (
-        <Section title="Transaction datum" key={i}>
-          <P>{TOPICS.datum}</P>
-          <PropBlock
-            title="Hash"
-            value={hash}
-            description={i == 0 ? TOPICS.datum_hash : ""}
-          />
-          <PropBlock title="Bytes" value={bytes} />
-          <PropBlock
-            title="JSON"
-            value={json}
-            description={i == 0 ? TOPICS.datum_json : ""}
-          />
-        </Section>
-      ))}
-      {witnesses?.plutusData.length === 0 && (
-        <EmptyBlock title="Transaction Datum" />
-      )}
+        </AccordionItem>
+      </Accordion>
+      <Accordion>
+        <AccordionItem
+          title={<h4 className="text-3xl">Witnesses</h4>}
+          textValue="Witnesses"
+          subtitle={<P>{TOPICS.witnesses}</P>}
+        >
+          {witnesses?.vkeyWitnesses.map(({ hash, key, signature }, i) => (
+            <Section title="Verification Key Witness" key={i}>
+              <PropBlock
+                title="Key"
+                value={key}
+                description={i == 0 ? TOPICS.vkey_witness : ""}
+              />
+              <PropBlock title="Hash" value={hash} />
+              <PropBlock title="Signature" value={signature} />
+            </Section>
+          ))}
+          {witnesses?.vkeyWitnesses.length === 0 && (
+            <EmptyBlock title="Verification Key Witness" />
+          )}
+          {witnesses?.plutusData.map(({ hash, bytes, json }, i) => (
+            <Section title="Transaction datum" key={i}>
+              <P>{TOPICS.datum}</P>
+              <PropBlock
+                title="Hash"
+                value={hash}
+                description={i == 0 ? TOPICS.datum_hash : ""}
+              />
+              <PropBlock title="Bytes" value={bytes} />
+              <PropBlock
+                title="JSON"
+                value={json}
+                description={i == 0 ? TOPICS.datum_json : ""}
+              />
+            </Section>
+          ))}
+          {witnesses?.plutusData.length === 0 && (
+            <EmptyBlock title="Transaction Datum" />
+          )}
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
