f29826fb769c3a43e50a58adbc918ea8883c47dd
sofia-bobbiesi <sofiabobbiesi@gmail.com>
2024-11-13 17:26:55 -0300

    fix: update response textarea

--- app/dsl/page.tsx
+++ app/dsl/page.tsx
@@ -9,9 +9,7 @@ import Loading from "../loading";
 export default function Index() {
   const { error, setError } = useUI();
   const [dsl, setDsl] = useState<string>("");
-  const [response, setResponse] = useState<string>(
-    "Parsed JSON will appear here",
-  );
+  const [response, setResponse] = useState<string>("");
 
   async function handleSubmit(e: React.ChangeEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -24,7 +22,7 @@ export default function Index() {
   }
 
   return (
-    <div className="p-6">
+    <div>
       <Header />
       <Suspense fallback={<Loading />}>
         {!isEmpty(error) && (
@@ -35,30 +33,29 @@ export default function Index() {
           onSubmit={handleSubmit}
           className="flex flex-col items-center gap-4"
         >
-          <div className="flex w-full max-w-4xl gap-6">
-            {/* Input DSL JSON Box */}
-            <div className="flex w-1/2 flex-col">
-              <label htmlFor="dsl" className="mb-2 text-xl font-semibold">
+          <div className="mt-16 flex w-full flex-grow gap-12">
+            <div className="ml-10 flex w-1/2 flex-col">
+              <label htmlFor="dsl" className="mb-6 text-xl font-semibold">
                 Input DSL JSON
               </label>
               <textarea
                 name="dsl"
                 value={dsl}
                 onChange={(e) => setDsl(e.target.value)}
-                placeholder="Enter JSON DSL here"
-                className="block h-40 w-full resize-none rounded-lg rounded-b-xl border-2 border-b-8 border-black bg-gray-100 p-3 text-lg text-black placeholder-gray-400 shadow shadow-black outline-none"
+                placeholder="Enter JSON here"
+                className="block min-h-[650px] w-full resize-none rounded-lg rounded-b-xl border-2 border-b-8 border-black bg-gray-100 p-3 text-lg text-black placeholder-gray-400 shadow shadow-black outline-none"
               />
             </div>
 
-            <div className="flex w-1/2 flex-col">
-              <label htmlFor="response" className="mb-2 text-xl font-semibold">
+            <div className="mr-10 flex w-1/2 flex-col">
+              <label htmlFor="response" className="mb-6 text-xl font-semibold">
                 Parsed Response
               </label>
               <textarea
                 name="response"
                 value={response}
-                placeholder="Parsed JSON response will appear here"
-                className="block h-40 w-full resize-none rounded-lg rounded-b-xl border-2 border-b-8 border-black bg-gray-100 p-3 text-lg text-black placeholder-gray-400 shadow shadow-black outline-none"
+                placeholder="Response will be displayed here"
+                className="block min-h-[650px] w-full resize-none rounded-lg rounded-b-xl border-2 border-b-8 border-black bg-gray-100 p-3 text-lg placeholder-gray-400 shadow shadow-black outline-none"
                 disabled
               />
             </div>
