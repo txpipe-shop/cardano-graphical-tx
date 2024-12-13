3f7bd536e44ab8e03a5201ea03000e7b4cd46751
sofia-bobbiesi <sofiabobbiesi@gmail.com>
2024-12-13 17:47:37 -0300

    feat: add tests for empty parameters and update common module

---
+++ napi-pallas/tests/common/empty_params.rs
@@ -0,0 +1,10 @@
+pub const EMPTY_PARAMS_JSON: &str = r#"{
+    "transaction": {
+      "inputs": [],
+      "outputs": []
+    }
+  }"#;
+
+pub const EMPTY_PARAMS_RES_HEX: &str = "84a400d90102800180020005a0a105a0f5d90103a100a0";
+
+pub const EMPTY_PARAMS_RES_DIAG: &str = r#"[{0: 258([]), 1: [], 2: 0}, {}, true, null]"#;
---
+++ napi-pallas/tests/test_cbor.rs
@@ -0,0 +1,11 @@
+mod common;
+use napi_pallas::cbor;
+
+#[test]
+fn test_empty_params() {
+  // cbor::parse_dsl(common::empty_params::EMPTY_PARAMS_JSON.to_owned());
+}
+#[test]
+fn test_all_params() {}
+#[test]
+fn test_100_inputs_outputs() {}
