94b884ae1bfe1d3beb51d69e65e6b71fc1b534bf
sofia-bobbiesi <sofiabobbiesi@gmail.com>
2025-01-06 17:33:20 -0300

    refactor: extract input preprocessing logic into a separate function

--- napi-pallas/src/cbor.rs
+++ napi-pallas/src/cbor.rs
@@ -27,43 +27,43 @@ use pallas_primitives::conway::{
 
 static SCHEMA_JSON: &str = include_str!("../.././docs/schema.json");
 
-pub fn preprocess_json(raw: &str) -> Result<Value, serde_json::Error> {
-  let mut res: Value = serde_json::from_str(raw)?;
+fn preprocess_inputs(inputs: &mut Value) {
+  if let Some(inputs_array) = inputs.as_array_mut() {
+    let mut used_indices: HashSet<u64> = inputs_array
+      .iter()
+      .filter_map(|input| input["index"].as_u64())
+      .collect();
 
-  let preprocess_inputs = |inputs: &mut Value| {
-    if let Some(inputs_array) = inputs.as_array_mut() {
-      let mut used_indices: HashSet<u64> = inputs_array
-        .iter()
-        .filter_map(|input| input["index"].as_u64())
-        .collect();
+    let mut next_index = *used_indices.iter().max().unwrap_or(&0) + 1;
 
-      let mut next_index = *used_indices.iter().max().unwrap_or(&0) + 1;
-
-      for input in inputs_array.iter_mut() {
-        if input["txHash"].is_null() {
-          input["txHash"] = json!(FIXED_HASH);
-        }
-
-        if input["index"].is_null() {
-          while used_indices.contains(&next_index) {
-            next_index += 1;
-          }
-          input["index"] = json!(next_index);
-          used_indices.insert(next_index);
-        }
+    for input in inputs_array.iter_mut() {
+      if input["txHash"].is_null() {
+        input["txHash"] = json!(FIXED_HASH);
       }
 
-      inputs_array.sort_by(|a, b| {
-        let tx_hash_a = a["txHash"].as_str().unwrap_or_default();
-        let tx_hash_b = b["txHash"].as_str().unwrap_or_default();
-
-        let index_a = a["index"].as_u64().unwrap_or_default();
-        let index_b = b["index"].as_u64().unwrap_or_default();
-
-        tx_hash_a.cmp(tx_hash_b).then(index_a.cmp(&index_b))
-      });
+      if input["index"].is_null() {
+        while used_indices.contains(&next_index) {
+          next_index += 1;
+        }
+        input["index"] = json!(next_index);
+        used_indices.insert(next_index);
+      }
     }
-  };
+
+    inputs_array.sort_by(|a, b| {
+      let tx_hash_a = a["txHash"].as_str().unwrap_or_default();
+      let tx_hash_b = b["txHash"].as_str().unwrap_or_default();
+
+      let index_a = a["index"].as_u64().unwrap_or_default();
+      let index_b = b["index"].as_u64().unwrap_or_default();
+
+      tx_hash_a.cmp(tx_hash_b).then(index_a.cmp(&index_b))
+    });
+  }
+}
+
+fn preprocess_json(raw: &str) -> Result<Value, serde_json::Error> {
+  let mut res: Value = serde_json::from_str(raw)?;
 
   if let Some(transaction) = res["transaction"].as_object_mut() {
     if let Some(inputs) = transaction.get_mut("inputs") {
