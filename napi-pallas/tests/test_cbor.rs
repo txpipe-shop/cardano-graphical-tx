55888b34103015bc18dfd7059bafac3e36c09f87
sofia-bobbiesi <sofiabobbiesi@gmail.com>
2025-01-06 15:06:10 -0300

    refactor: simplify error handling and result extraction in CBOR tests

--- napi-pallas/tests/test_cbor.rs
+++ napi-pallas/tests/test_cbor.rs
@@ -10,29 +10,22 @@ fn test_empty_params() {
   let empty_params_json = EMPTY_PARAMS_JSON.to_owned();
 
   let result = cbor::parse_dsl(empty_params_json);
-  let parsed_result: serde_json::Value = serde_json::from_str(&result).unwrap();
 
   assert!(
-    !parsed_result.get("error").is_some(),
+    result.error.is_empty(),
     "parse_dsl returned an error: {}",
-    parsed_result.get("error").unwrap()
+    result.error
   );
 
-  let cbor_hex = parsed_result.get("cbor_hex").unwrap().as_str().unwrap();
+  let dsl_res = result.dsl_res.unwrap();
 
   assert_eq!(
-    cbor_hex, EMPTY_PARAMS_RES_HEX,
+    dsl_res.cbor_hex, EMPTY_PARAMS_RES_HEX,
     "CBOR hex does not match the expected result"
   );
 
-  let cbor_diag = parsed_result
-    .get("cbor_diagnostic")
-    .unwrap()
-    .as_str()
-    .unwrap();
-
   assert_eq!(
-    cbor_diag, EMPTY_PARAMS_RES_DIAG,
+    dsl_res.cbor_diagnostic, EMPTY_PARAMS_RES_DIAG,
     "CBOR diagnostic does not match the expected result"
   );
 }
@@ -42,29 +35,22 @@ fn test_all_params() {
   let all_params_json = ALL_PARAMS_JSON.to_owned();
 
   let result = cbor::parse_dsl(all_params_json);
-  let parsed_result: serde_json::Value = serde_json::from_str(&result).unwrap();
 
   assert!(
-    !parsed_result.get("error").is_some(),
+    result.error.is_empty(),
     "parse_dsl returned an error: {}",
-    parsed_result.get("error").unwrap()
+    result.error
   );
 
-  let cbor_hex = parsed_result.get("cbor_hex").unwrap().as_str().unwrap();
+  let dsl_res = result.dsl_res.unwrap();
 
   assert_eq!(
-    cbor_hex, ALL_PARAMS_RES_HEX,
+    dsl_res.cbor_hex, ALL_PARAMS_RES_HEX,
     "CBOR hex does not match the expected result"
   );
 
-  let cbor_diag = parsed_result
-    .get("cbor_diagnostic")
-    .unwrap()
-    .as_str()
-    .unwrap();
-
   assert_eq!(
-    cbor_diag, ALL_PARAMS_RES_DIAG,
+    dsl_res.cbor_diagnostic, ALL_PARAMS_RES_DIAG,
     "CBOR diagnostic does not match the expected result"
   );
 }
@@ -73,29 +59,22 @@ fn test_100_inputs_outputs() {
   let input_output_json = INPUT_OUTPUT_JSON.to_owned();
 
   let result = cbor::parse_dsl(input_output_json);
-  let parsed_result: serde_json::Value = serde_json::from_str(&result).unwrap();
 
   assert!(
-    !parsed_result.get("error").is_some(),
+    result.error.is_empty(),
     "parse_dsl returned an error: {}",
-    parsed_result.get("error").unwrap()
+    result.error
   );
 
-  let cbor_hex = parsed_result.get("cbor_hex").unwrap().as_str().unwrap();
+  let dsl_res = result.dsl_res.unwrap();
 
   assert_eq!(
-    cbor_hex, INPUT_OUTPUT_RES_HEX,
+    dsl_res.cbor_hex, INPUT_OUTPUT_RES_HEX,
     "CBOR hex does not match the expected result"
   );
 
-  let cbor_diag = parsed_result
-    .get("cbor_diagnostic")
-    .unwrap()
-    .as_str()
-    .unwrap();
-
   assert_eq!(
-    cbor_diag, INPUT_OUTPUT_RES_DIAG,
+    dsl_res.cbor_diagnostic, INPUT_OUTPUT_RES_DIAG,
     "CBOR diagnostic does not match the expected result"
   );
 }
