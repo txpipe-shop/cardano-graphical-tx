3754932281a112ed952283d39d87d9c525cdf749
Nicolas Ludueña <nicolas.luduena@txpipe.io>
2025-12-11 00:38:57 -0300

    feat: dependencies for parsing cbor and utxorpc custom sdk

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import wasm from 'vite-plugin-wasm';
 
-export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+export default defineConfig(({ isSsrBuild }) => ({
+  plugins: [tailwindcss(), sveltekit(), wasm()],
   ssr: {
     noExternal: ['bits-ui']
+  },
+  resolve: {
+    alias: {
+      '@sdk/grpcTransport': isSsrBuild
+        ? '/src/lib/sdk/utxorpc/grpcTransport.node.ts'
+        : '/src/lib/sdk/utxorpc/grpcTransport.web.ts'
+    }
   },
   define: {
     'process.env': {}
   }
-});
+}));
