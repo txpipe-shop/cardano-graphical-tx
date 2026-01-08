00af3e898bd4e16b8177dff581cdf129776a59d0
Nicolas Ludueña <luduena.nicolas.victorio@gmail.com>
2026-01-08 16:38:57 -0300

    feat: exhaustive questions over fields in addresses endpoint

--- apps/api/src/routes/addresses.ts
+++ apps/api/src/routes/addresses.ts
@@ -46,27 +46,34 @@ export function addressesRoutes(app: FastifyInstance) {
       const funds = await provider.getAddressFunds({ address });
 
       const balanceLovelace = Number(funds.value[Unit('lovelace')]?.toString() || 0);
+      const balanceAda = balanceLovelace / 10 ** 6;
 
-      // TODO: API needs to have pagination over this (I'd do this with different endpoints)
+      // TODO: API needs to have pagination over this (recommendation: do this with different endpoints)
       const { data: txs, total: totalTxs } = await provider.getTxs({
         limit: 100n,
         query: { address: Address(rawAddress) }
       });
 
-      // TODO: API needs to have pagination over this (I'd do this with different endpoints)
+      // TODO: API needs to have pagination over this (recommendation: do this with different endpoints)
       const { data: outputs, total: totalUtxos } = await provider.getAddressUTxOs({
         query: { address },
         limit: 100n
       });
+
       const transactions: AddressRes['transactions'] = txs.map((tx) => {
         return {
-          amount_ada: Number(123),
-          amount_lovelace: Number(123),
+          // TODO: What amount of ADA (in inputs or outputs?)
+          amount_ada: 123,
+          amount_lovelace: 123,
           block_height: Number(tx.block?.height),
           hash: tx.hash,
-          received_ada: Number(123),
-          sent_ada: Number(123),
-          slot: Number(123),
+          // TODO: received ADA in which address?
+          received_ada: 123,
+          // TODO: sent ADA to which address?
+          sent_ada: 123,
+
+          // TODO: information not shown in UI
+          slot: 0,
           timestamp: tx.createdAt ? new Date(tx.createdAt).toISOString() : null,
           tx_index: Number(tx.indexInBlock),
           // TODO: ask what type means for a tx
@@ -75,13 +82,14 @@ export function addressesRoutes(app: FastifyInstance) {
       });
 
       const utxos: AddressRes['utxos'] = outputs.map((x) => ({
-        amount_ada: Number(x.coin),
+        amount_ada: Number(x.coin) / 10 ** 6,
         amount_lovelace: Number(x.coin),
-        block_height: 0,
-        slot: 0,
         output_index: Number(x.outRef.index),
         tx_hash: x.outRef.hash,
-        utxo_id: `${x.outRef.hash}#${x.outRef.index}`
+        utxo_id: `${x.outRef.hash}#${x.outRef.index}`,
+        // TOOD: information not used by UI
+        block_height: 0,
+        slot: 0
       }));
 
       const tokens: AddressRes['tokens'] = Object.entries(funds.value).map(([u, amount]) => {
@@ -90,31 +98,41 @@ export function addressesRoutes(app: FastifyInstance) {
           asset_name: assetNameFromUnit(unit),
           asset_name_hex: assetNameFromUnit(unit),
           policy_id: policyFromUnit(unit),
+          // TODO: balance formatted into what
           balance_formatted: null,
+          // TODO: get from token registry?
           decimals: null,
           fingerprint: fingerprintFromUnit(unit),
           // why multiple ways of specifying amounts?
           balance: amount.toString(),
           quantity: amount.toString(),
-          name: null
+          // TODO: just utf8 of hex name or also support cip68?
+          name: null,
+          // EVM fields
+          symbol: undefined,
+          token_address: undefined,
+          token_name: undefined,
+          token_symbol: undefined,
+          value: undefined
         };
       });
 
       return {
-        address: address,
+        address,
         balance_lovelace: balanceLovelace,
         tx_count: Number(totalTxs),
-        balance_ada: 123,
+        balance_ada: balanceAda,
         address_bech32: isBase58(address) ? null : hexToBech32(HexString(address), 'addr'),
+        unspent_utxo_count: Number(totalUtxos),
+        tokens,
+        transactions,
+        utxos,
+        // TODO: fields are not being used in UI
         first_seen_height: undefined,
         first_seen_slot: undefined,
         last_seen_height: undefined,
         last_seen_slot: undefined,
-        total_utxo_count: undefined,
-        unspent_utxo_count: Number(totalUtxos),
-        tokens,
-        transactions,
-        utxos
+        total_utxo_count: undefined
       };
     }
   );
