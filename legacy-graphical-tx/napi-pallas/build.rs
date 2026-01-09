c68a34ca95277d2c862818d0ebc49624e4fc26cb
Nicolas Ludueña <nicolas.luduena@txpipe.io>
2026-01-09 17:58:32 -0300

    feat: added laceanatomy web as web app

--- legacy-graphical-tx/napi-pallas/build.rs
+++
@@ -1,8 +0,0 @@
-extern crate napi_build;
-
-fn main() {
-  if cfg!(feature = "test-build") {
-    println!("cargo:rustc-link-lib=dylib=node");
-  }
-  napi_build::setup();
-}
--- legacy-graphical-tx/napi-pallas/npm/linux-arm64-gnu/README.md
+++
@@ -1,3 +0,0 @@
-# `napi-pallas-linux-arm64-gnu`
-
-This is the **aarch64-unknown-linux-gnu** binary for `napi-pallas`
