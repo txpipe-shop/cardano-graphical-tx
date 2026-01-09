519b3ae4e95bd45335fec0853ddf739fc00e601f
Nicolas Ludueña <nicolas.luduena@txpipe.io>
2026-01-09 18:18:11 -0300

    chore: build without warnings about missing binaries

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -3,7 +3,7 @@ await import("./app/env.mjs");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['napi-pallas']
+    serverComponentsExternalPackages: ["@laceanatomy/napi-pallas"],
   },
   reactStrictMode: true,
   images: {
@@ -18,12 +18,26 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.externals = [...config.externals, { canvas: "canvas" }]; // required to make Konva & react-konva work
     config.module.rules.push({
       test: /\.node$/,
-      use: [{ loader: "nextjs-node-loader" }]
-    })
+      use: [{ loader: "nextjs-node-loader" }],
+    });
+
+    config.ignoreWarnings = [
+      ...(config.ignoreWarnings || []),
+      {
+        module: /napi-pallas/,
+        message: /Can't resolve/,
+      },
+      {
+        module: /napi-pallas/,
+        message:
+          /Critical dependency: the request of a dependency is an expression/,
+      },
+    ];
+
     return config;
   },
 };
