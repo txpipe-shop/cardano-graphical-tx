06c4c117ff64418475c8b878609dc681c4dc748d
FranZavalla <franzavalla85@gmail.com>
2026-01-16 14:57:26 -0300

    feat: added grpc web transport

--- apps/web/server/api/u5c-provider.ts
+++ apps/web/server/api/u5c-provider.ts
@@ -1,5 +1,6 @@
 import { U5CProvider } from "@laceanatomy/cardano-provider-u5c";
 import { createGrpcTransport as createGrpcTransportNode } from "@laceanatomy/utxorpc-sdk/transport/node";
+import { createGrpcTransport as createGrpcTransportWeb } from "@laceanatomy/utxorpc-sdk/transport/web";
 
 function createTransportNode(
   baseUrl: string,
@@ -21,8 +22,30 @@ function createTransportNode(
   });
 }
 
-export function getU5CProvider(): U5CProvider {
+function createTransportWeb(baseUrl: string, headers?: Record<string, string>) {
+  return createGrpcTransportWeb({
+    baseUrl,
+    interceptors: headers
+      ? [
+          (next) => async (req) => {
+            for (const [key, value] of Object.entries(headers)) {
+              req.header.set(key, value);
+            }
+            return next(req);
+          },
+        ]
+      : [],
+  });
+}
+
+export function getU5CProviderNode(): U5CProvider {
   return new U5CProvider({
     transport: createTransportNode("http://localhost:50051"),
+  });
+}
+
+export function getU5CProviderWeb(): U5CProvider {
+  return new U5CProvider({
+    transport: createTransportWeb("http://localhost:50051"),
   });
 }
