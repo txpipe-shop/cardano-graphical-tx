91a3409fbc888e7e16117dbfdfc2417ed062fb96
FranZavalla <franzavalla85@gmail.com>
2026-01-19 15:52:21 -0300

    feat: moved u5c web provider

---
+++ apps/web/app/_utils/u5c-provider-web.ts
@@ -0,0 +1,24 @@
+import { U5CProvider } from "@laceanatomy/cardano-provider-u5c";
+import { createGrpcTransport } from "@laceanatomy/utxorpc-sdk/transport/web";
+
+function createTransportWeb(baseUrl: string, headers?: Record<string, string>) {
+  return createGrpcTransport({
+    baseUrl,
+    interceptors: headers
+      ? [
+        (next) => async (req) => {
+          for (const [key, value] of Object.entries(headers)) {
+            req.header.set(key, value);
+          }
+          return next(req);
+        },
+      ]
+      : [],
+  });
+}
+
+export function getU5CProviderWeb(): U5CProvider {
+  return new U5CProvider({
+    transport: createTransportWeb("http://localhost:50051"),
+  });
+}
