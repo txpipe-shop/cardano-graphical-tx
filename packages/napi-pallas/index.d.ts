5daa166f05e7b5cf79be72f10ae45b3d21ae3d5d
Nicolas Ludueña <luduena.nicolas.victorio@gmail.com>
2026-01-22 13:52:27 -0300

    fix: prettier and lint

--- packages/napi-pallas/index.d.ts
+++ packages/napi-pallas/index.d.ts
@@ -77,7 +77,12 @@ export interface Datum {
   json: string
 }
 
-export declare function downloadBlock(nodeUrl: string, magic: number, slot: number, hash: string): Promise<Array<number>>
+export declare function downloadBlock(
+  nodeUrl: string,
+  magic: number,
+  slot: number,
+  hash: string,
+): Promise<Array<number>>
 
 export interface ExUnits {
   mem: number
