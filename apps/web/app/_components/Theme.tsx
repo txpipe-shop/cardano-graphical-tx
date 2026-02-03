8f1d9dba561d1371234fe7a9f2c98705be3df262
FranZavalla <franzavalla85@gmail.com>
2026-02-03 12:14:06 -0300

    fix: hydration error

--- apps/web/app/_components/Theme.tsx
+++ apps/web/app/_components/Theme.tsx
@@ -2,9 +2,30 @@
 
 import { Button } from "@heroui/react";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export const ThemeButton = () => {
   const { theme, resolvedTheme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // This solution avoid 2 hydration errors
+  // TODO: find a better solution
+  if (!mounted) {
+    return (
+      <Button
+        variant="flat"
+        className="p-5 font-mono text-lg shadow-md"
+        isIconOnly
+        isDisabled
+      >
+        ◯
+      </Button>
+    );
+  }
 
   const currentTheme = resolvedTheme ?? theme ?? "light";
   const isDark = currentTheme === "dark";
